fc6f532197d8b40cb35d717822ad7052695ea95e
wmore <ywgjwangyue@sina.com>
2016-07-13 23:21:52 +0700

    wangyue
    service finish

--- src/test/java/org/seckill/service/SeckillServiceTest.java
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.seckill.dto.Exposer;
 import org.seckill.dto.SeckillExecution;
 import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,7 +65,35 @@ public class SeckillServiceTest {
         long id = 1000;
         long phone = 1824123412;
         String md5 = "fba4327bfc1317ccb934c5744ed59cbe";
-        SeckillExecution seckillExecution = seckillService.executeSeckill(id, phone, md5);
-        logger.info("seckillExecution={}",seckillExecution);
+        try {
+            SeckillExecution seckillExecution = seckillService.executeSeckill(id, phone, md5);
+            logger.info("result={}", seckillExecution);
+        } catch (RepeatKillException e) {
+            logger.error(e.getMessage());
+        } catch (SeckillCloseException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void testSeckillLogic() throws Exception {
+        long id = 1002;
+        Exposer exposer = seckillService.exportSeckillUrl(id);
+        logger.info("exposer = {}", exposer);
+        if (exposer.isExposed()) {
+            long phone = 1824000001;
+            String md5 = exposer.getMd5();
+            try {
+                SeckillExecution execution = seckillService.executeSeckill(id, phone, md5);
+                logger.info("result = {}", execution);
+            } catch (RepeatKillException e) {
+                logger.error(e.getMessage());
+            } catch (SeckillCloseException e) {
+                logger.error(e.getMessage());
+            }
+        } else {
+            logger.warn("exposer is not open!");
+        }
     }
 }
